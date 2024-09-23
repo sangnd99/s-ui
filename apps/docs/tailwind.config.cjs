3702fb66228270f1e5dd74404dfd8cae8dd0c456
Nguyễn Sang <sang.nguyen@spiritlabs.co>
2024-09-23 21:50:53 +0700

    feat(docs): init tailwindcss

--- apps/docs/tailwind.config.cjs
+++ apps/docs/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./stories/**/*.stories.{html,js,ts,jsx,tsx}'],
+    content: [
+    './stories/**/*.stories.{html,js,ts,jsx,tsx}',
+    '../../packages/**/*.{html,js,ts,jsx,tsx}'
+  ],
     theme: {
         extend: {},
     },
